e3eab3b36c6d77e11a487b6edba8f447ffeb8998
joeycarlos <jclaguio@gmail.com>
2019-10-18 11:47:30 -0700

    Add splash screen UI

---
+++ Assets/SplashUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashUIManager : MonoBehaviour
+{
+    private static SplashUIManager _instance;
+
+    public static SplashUIManager Instance {
+        get {
+            return _instance;
+        }
+    }
+
+    public Text gameTitle;
+    public Text author;
+    public Text date;
+    public Text description;
+    public Text context;
+
+    void Awake() {
+        _instance = this;
+    }
+}
